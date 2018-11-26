9a8d0309dcb1fe2514e4681839e486e1d909ddd6
AnngelNts <Anngel.Nts@gmail.com>
2018-11-26 11:29:46 -0500

    Update DEgreso

--- CooperativaApp/CooperativaApp.Datos/DEgreso.cs
+++ CooperativaApp/CooperativaApp.Datos/DEgreso.cs
@@ -3,6 +3,7 @@ using CooperativaApp.Entidades;
 using MySql.Data.MySqlClient;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +24,48 @@ namespace CooperativaApp.Datos
             parameters[5] = new MYSQLParameter("@Fecha_De_Registro", obj.Id_Usuario, MySqlDbType.Date);
             Response = ConexionMySql.ExecuteProcedureNonQuery("USP_Add_Egreso", parameters);
             return Response;
+        }
+
+        public List<Egreso> Listar()
+        {
+            List<Egreso> List = new List<Egreso>();
+            try
+            {
+                DataTable data = new DataTable();
+                data = ConexionMySql.ExecuteProcedureData("USP_ToList_Egresos");
+                foreach (DataRow row in data.Rows)
+                {
+                    Egreso be = new Egreso
+                    {
+
+                    };
+                    List.Add(be);
+                }
+            }
+            catch
+            {
+                Console.WriteLine("No se encontro Procedimiento Almacenado");
+            }
+            return List;
+        }
+
+        public Egreso Seleccionar(int Identificador)
+        {
+            try
+            {
+                MYSQLParameter[] parameters = new MYSQLParameter[1];
+                parameters[0] = new MYSQLParameter("@Tipo_Documento", Identificador, MySqlDbType.Int32);
+                DataRow row = ConexionMySql.ExecuteProcedureData("USP_Select_Egreso", parameters).Rows[0];
+                Egreso be = new Egreso
+                {
+                };
+                return be;
+            }
+            catch
+            {
+                Egreso be = new Egreso();
+                return be;
+            }
         }
     }
 }
